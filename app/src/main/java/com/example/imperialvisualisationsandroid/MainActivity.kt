ad878b8c2161e0d389caa8ba41abb3f32b841f8d
Vedant Varshney <vv218@ic.ac.uk>
2019-07-29 20:28:38 +0100

    Search Functionality
    Search added

--- app/src/main/java/com/example/imperialvisualisationsandroid/MainActivity.kt
+++ app/src/main/java/com/example/imperialvisualisationsandroid/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.example.imperialvisualisationsandroid
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.SearchView
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Filterable
 import com.google.gson.GsonBuilder
+import com.klinker.android.peekview.PeekViewActivity
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import java.io.IOException
 
-class MainActivity : AppCompatActivity() {
+public class MainActivity : AppCompatActivity() {
+
+    // data variable - changed only once during data decode
+    var data: DataModel = DataModel(Visualisations = emptyList())
+
+    private var searchView: SearchView? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +41,8 @@ class MainActivity : AppCompatActivity() {
 
     fun decodeJSON() {
 
+        //var data: DataModel?
+
         val jsonURL = "https://raw.githubusercontent.com/VedantVarshney/VisualisationsPersonal/master/DataModel"
 
         val request = Request.Builder().url(jsonURL).build()
@@ -42,7 +58,7 @@ class MainActivity : AppCompatActivity() {
                 val gson = GsonBuilder().create()
 
                 //class.java??
-                val data = gson.fromJson(jsonBody, DataModel::class.java)
+                data = gson.fromJson(jsonBody, DataModel::class.java)
                 println("JSON FETCH SUCCESSFUL")
 
                 //cannot return data here as return needs to be unit (cannot specify model to DataModel)
@@ -58,5 +74,49 @@ class MainActivity : AppCompatActivity() {
                 println(e)
             }
         })
+
+
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.options_menu, menu)
+
+
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        // ???
+        searchView = menu?.findItem(R.id.search)?.actionView as SearchView
+
+        searchView!!.setSearchableInfo(searchManager.getSearchableInfo(componentName))
+
+        searchView!!.maxWidth = Integer.MAX_VALUE
+
+        searchView!!.setOnQueryTextListener(object: SearchView.OnQueryTextListener {
+
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                //MainAdapter(data).filter.filter(query)
+
+                (recyclerView_main.adapter as Filterable).filter.filter(query)
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                //MainAdapter(data).filter.filter(newText)
+
+                (recyclerView_main.adapter as Filterable).filter.filter(newText)
+                return false
+            }
+        })
+
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+
+        val id = item?.itemId
+        return if (id == R.id.search){
+            true
+        } else super.onOptionsItemSelected(item)
+    }
+
 }
--- app/src/main/java/com/example/imperialvisualisationsandroid/MainAdapter.kt
+++ app/src/main/java/com/example/imperialvisualisationsandroid/MainAdapter.kt
@@ -7,16 +7,20 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filter
+import android.widget.Filterable
 import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.visualisation_cell.view.*
 
-class MainAdapter(val data: DataModel) : RecyclerView.Adapter<ViewHolder>() {
+class MainAdapter(val data: DataModel) : RecyclerView.Adapter<ViewHolder>(), Filterable {
 
     val visualisations = data.Visualisations
 
+    var searchVisualisations = data.Visualisations.toMutableList()
+
     override fun getItemCount(): Int {
-        return visualisations.count()
+        return searchVisualisations.count()
     }
 
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
@@ -35,10 +39,10 @@ class MainAdapter(val data: DataModel) : RecyclerView.Adapter<ViewHolder>() {
 
         holder.itemView.setBackgroundColor(Color.WHITE)
 
-        val selectedVisualisation = visualisations.get(position)
+        val selectedVisualisation = searchVisualisations.get(position)
 
-        holder.view.titleTextView.text = visualisations[position].name
-        holder.view.infoTextView.text = visualisations[position].info
+        holder.view.titleTextView.text = searchVisualisations[position].name
+        holder.view.infoTextView.text = searchVisualisations[position].info
 
         //PICASSO
         //Picasso.get().load(visualisations[position].imageURL).into(holder.view.VisualisationImageView)
@@ -47,12 +51,50 @@ class MainAdapter(val data: DataModel) : RecyclerView.Adapter<ViewHolder>() {
         //GLIDE
         val context = holder.view.context
         Glide.with(context)
-            .load(visualisations[position].imageURL)
+            .load(searchVisualisations[position].imageURL)
             .into(holder.view.VisualisationImageView)
 
         holder.selectedVisualisation = selectedVisualisation
 
+    }
 
+
+    override fun getFilter(): Filter {
+        return object: Filter() {
+
+            override fun performFiltering(p0: CharSequence?): FilterResults {
+                val searchQuery = p0.toString().toLowerCase()
+
+                var filteredVisualisations = ArrayList<Visualisation>()
+
+                if (searchQuery.isEmpty()) {
+                    filteredVisualisations.addAll(visualisations)
+                } else {
+                    for (vis in visualisations) {
+
+                        var visString = (vis.name + " " + vis.info + " " + vis.tags).toLowerCase()
+
+                        if (visString.contains(searchQuery)) {
+                            filteredVisualisations.add(vis)
+                        }
+                    }
+
+//                    searchVisualisations.clear()
+//                    searchVisualisations.addAll(filteredVisualisations)
+
+                }
+
+                val filterResults = FilterResults()
+                filterResults.count = filteredVisualisations.size
+                filterResults.values = filteredVisualisations
+                return filterResults
+            }
+
+            override fun publishResults(p0: CharSequence?, p1: FilterResults?) {
+                searchVisualisations = p1?.values as ArrayList<Visualisation>
+                notifyDataSetChanged()
+            }
+        }
     }
 }
 
@@ -73,10 +115,11 @@ class ViewHolder(val view: View, var selectedVisualisation: Visualisation? = nul
             view.context.startActivity(intent)
         }
 
-        view.setOnLongClickListener {
-            val intent = Intent(view.context, GIFDetailActivity::class.java)
-            view.context.startActivity(intent)
-        }
+//        view.setOnLongClickListener {
+////            val intent = Intent(view.context, GIFDetailActivity::class.java)
+////            view.context.startActivity(intent)
+////        }
     }
 
 }
+
